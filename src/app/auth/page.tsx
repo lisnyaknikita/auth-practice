a92b129b488186402ccd2ae75f9685bf327582e8
nlisnyak <lisnyaknikita@gmail.com>
2024-10-27 14:54:45 +0200

    fix: fixed deploy errors

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,6 @@
-import type { Metadata } from 'next'
-
 import { AuthScreen } from '@/features/auth/components/auth-screen/auth-screen'
-import classes from './auth-page.module.scss'
 
-export const metadata: Metadata = {
-	title: '',
-	description: '',
-}
+import classes from './auth-page.module.scss'
 
 export default function AuthPage() {
 	return (
